a1a0467b86166a73a19c9fca0f15f0147af2d230
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2019-12-30 16:05:51 +0800

    minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
-const int floatSwtichPin = 5; // should have 10k pull-up resistor
+const int floatSwtichPin = 5;  // should have 10k pull-up resistor
 const int moistureReadPin = 4; // should have 10k pull-up resistor
-const int moistureOnPin = 13; // should have 10k pull-down resistor
-const int waterPumpPin = 14; // should have current limiting resistor, pull-down resistor
-int delaySec = 10; // seconds
+const int moistureOnPin = 13;  // should have 10k pull-down resistor
+const int waterPumpPin = 14;   // should have current limiting resistor, pull-down resistor
+int delaySec = 10;             // seconds
 int delayCount = 0;
 int forceStopSec = 120; // seconds
-int sleepTimeSec = 30; // seconds
+int sleepTimeSec = 30;  // seconds
 
 void setup()
 {
@@ -28,29 +28,27 @@ void loop()
   Serial.println();
 
   // float switch == low means has water
-  if(floatSwitch == LOW)
+  if (floatSwitch == LOW)
   {
 
     Serial.println("Water is avaiable.");
 
     // Power on moisture sensor
     digitalWrite(moistureOnPin, HIGH);
-    
+
     // milli seconds, make sure the sensor will have time to turn on
     delay(20);
 
     // read soil moisture value
     int moistureRead = digitalRead(moistureReadPin);
-
-    // Power off moisture sensor
-    digitalWrite(moistureOnPin, LOW);
-
-    Serial.println("Starting moisture measurement.");
     Serial.print("Moisture logic level:");
     Serial.print(moistureRead);
     Serial.println();
 
-    if(moistureRead == HIGH)
+    // Power off moisture sensor
+    digitalWrite(moistureOnPin, LOW);
+
+    if (moistureRead == HIGH)
     {
       needWatering = true;
     }
@@ -62,13 +60,13 @@ void loop()
 
   // force stop if watering time is more than X seconds
   // in case the moisture sensor or something else out of control.
-  if(delaySec*delayCount > forceStopSec)
+  if (delaySec * delayCount > forceStopSec)
   {
     Serial.println("Force stop watering");
     needWatering = false;
   }
 
-  if(needWatering == true)
+  if (needWatering == true)
   {
 
     // water pump on
@@ -81,7 +79,7 @@ void loop()
     Serial.println();
 
     // delay milli seconds
-    delay(delaySec*1000);
+    delay(delaySec * 1000);
 
     delayCount++;
   }
@@ -99,7 +97,6 @@ void loop()
     Serial.println();
 
     // into deep sleep mode
-    ESP.deepSleep(sleepTimeSec*1e6);
+    ESP.deepSleep(sleepTimeSec * 1e6);
   }
-
 }
