91c340010020857b44f1854cf386d4bf49fe443a
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2020-01-07 16:27:14 +0800

    change indent size to 4

--- src/main.cpp
+++ src/main.cpp
@@ -6,97 +6,97 @@ const int moistureOnPin = 13;  // should have 10k pull-down resistor
 const int waterPumpPin = 14;   // should have current limiting resistor, pull-down resistor
 int delaySec = 10;             // seconds
 int delayCount = 0;
-int forceStopSec = 120; // seconds
-uint32_t sleepTimeSec = 86400;  // seconds
+int forceStopSec = 120;        // seconds
+uint32_t sleepTimeSec = 86400; // seconds
 
 void setup()
 {
-  Serial.begin(115200);
-  pinMode(floatSwtichPin, INPUT);
-  pinMode(moistureReadPin, INPUT);
-  pinMode(moistureOnPin, OUTPUT);
-  pinMode(waterPumpPin, OUTPUT);
+    Serial.begin(115200);
+    pinMode(floatSwtichPin, INPUT);
+    pinMode(moistureReadPin, INPUT);
+    pinMode(moistureOnPin, OUTPUT);
+    pinMode(waterPumpPin, OUTPUT);
 }
 
 void loop()
 {
-  bool needWatering = false;
+    bool needWatering = false;
 
-  // Read the water level switch to see if water is avaiable
-  bool floatSwitch = digitalRead(floatSwtichPin);
+    // Read the water level switch to see if water is avaiable
+    bool floatSwitch = digitalRead(floatSwtichPin);
 
-  Serial.println();
-
-  // float switch == low means has water
-  if (floatSwitch == LOW)
-  {
-
-    Serial.println("Water is avaiable.");
-
-    // Power on moisture sensor
-    digitalWrite(moistureOnPin, HIGH);
-
-    // milli seconds, make sure the sensor will have time to turn on
-    delay(20);
-
-    // read soil moisture value
-    int moistureRead = digitalRead(moistureReadPin);
-    Serial.print("Moisture logic level:");
-    Serial.print(moistureRead);
     Serial.println();
 
-    // Power off moisture sensor
-    digitalWrite(moistureOnPin, LOW);
-
-    if (moistureRead == HIGH)
+    // float switch == low means has water
+    if (floatSwitch == LOW)
     {
-      needWatering = true;
+
+        Serial.println("Water is avaiable.");
+
+        // Power on moisture sensor
+        digitalWrite(moistureOnPin, HIGH);
+
+        // milli seconds, make sure the sensor will have time to turn on
+        delay(20);
+
+        // read soil moisture value
+        int moistureRead = digitalRead(moistureReadPin);
+        Serial.print("Moisture logic level:");
+        Serial.print(moistureRead);
+        Serial.println();
+
+        // Power off moisture sensor
+        digitalWrite(moistureOnPin, LOW);
+
+        if (moistureRead == HIGH)
+        {
+            needWatering = true;
+        }
     }
-  }
-  else
-  {
-    Serial.println("Water is not avaiable.");
-  }
+    else
+    {
+        Serial.println("Water is not avaiable.");
+    }
 
-  // force stop if watering time is more than X seconds
-  // in case the moisture sensor or something else out of control.
-  if (delaySec * delayCount > forceStopSec)
-  {
-    Serial.println("Force stop watering");
-    needWatering = false;
-  }
+    // force stop if watering time is more than X seconds
+    // in case the moisture sensor or something else out of control.
+    if (delaySec * delayCount > forceStopSec)
+    {
+        Serial.println("Force stop watering");
+        needWatering = false;
+    }
 
-  if (needWatering == true)
-  {
+    if (needWatering == true)
+    {
 
-    // water pump on
-    digitalWrite(waterPumpPin, HIGH);
+        // water pump on
+        digitalWrite(waterPumpPin, HIGH);
 
-    Serial.print("Pumping for ");
-    // Serial.print("Delay ");
-    Serial.print(delaySec);
-    Serial.print(" seconds");
-    Serial.println();
+        Serial.print("Pumping for ");
+        // Serial.print("Delay ");
+        Serial.print(delaySec);
+        Serial.print(" seconds");
+        Serial.println();
 
-    // delay milli seconds
-    delay(delaySec * 1000);
+        // delay milli seconds
+        delay(delaySec * 1000);
 
-    delayCount++;
-  }
-  else
-  {
+        delayCount++;
+    }
+    else
+    {
 
-    Serial.println("No pumping");
+        Serial.println("No pumping");
 
-    // close water pump whether it's on or not
-    digitalWrite(waterPumpPin, LOW);
+        // close water pump whether it's on or not
+        digitalWrite(waterPumpPin, LOW);
 
-    Serial.print("Deep sleep for ");
-    Serial.print(sleepTimeSec);
-    Serial.print(" seconds");
-    Serial.println();
+        Serial.print("Deep sleep for ");
+        Serial.print(sleepTimeSec);
+        Serial.print(" seconds");
+        Serial.println();
 
-    // into deep sleep mode
-    ESP.deepSleep(sleepTimeSec * 1e6);
-  }
+        // into deep sleep mode
+        ESP.deepSleep(sleepTimeSec * 1e6);
+    }
 }
